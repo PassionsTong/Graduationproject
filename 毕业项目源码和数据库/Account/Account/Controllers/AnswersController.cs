b083650c1f320304d2afed3ff4cd758069087af2
PassionsTong <867366548@qq.com>
2020-06-27 20:38:19 +0800

    更新

--- 毕业项目源码和数据库/Account/Account/Controllers/AnswersController.cs
+++ 毕业项目源码和数据库/Account/Account/Controllers/AnswersController.cs
@@ -105,5 +105,64 @@ namespace Account.Controllers
             return RedirectToAction("MyAnswer");
         
         }
+        //老师审卷
+        public ActionResult TeAnswer()
+        {
+            return View(db.Answer.Include("Student").ToList());
+
+        }
+        //答题试卷
+        public ActionResult TeAnswerDetail(int id) 
+        {
+            //Answer answer = db.Answer.Find(id);
+            Answer answer = db.Answer.Include("Paper").Include("Student").First(a => a.AnswerID == id);
+            if (answer==null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.answer = answer;
+            ViewBag.Details = db.Detail.Include("Topic").Include("Answer").Where(a => a.AnswerID == id).ToList();
+            return View(answer);
+        
+        }
+        //提交审核
+        public ActionResult Verify(int id) 
+        {
+            Answer answer = db.Answer.Find(id);
+            foreach (Detail item in answer.Detail)
+            {
+                if (item.DetailAnswer==item.Topic.TopicAnswer)
+                {
+                    answer.AnswerScore += item.Topic.TopicScore;
+                }
+            }
+            answer.AnswerState = 2;
+            answer.BatchTime = DateTime.Now;
+            db.Entry(answer).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("TeAnswer");
+        
+        }
+        //学生试卷
+        public ActionResult MyAnswer() 
+        {
+            int sid= (int)Session["LoginID"];
+            var answers = db.Answer.Include("Paper").Include("Student").Where(a => a.StuID == sid).ToList();
+            return View(answers);
+
+        }
+        public ActionResult MyAnswerDetail(int id) 
+        {
+            //Answer answer = db.Answer.Find(id);
+            Answer answer = db.Answer.Include("Paper").Include("Student").First(a => a.AnswerID == id);
+            if (answer==null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.answer = answer;
+            ViewBag.Details = db.Detail.Include("Topic").Include("Answer").Where(a => a.AnswerID == id).ToList();
+            return View(answer);
+        
+        }
     }
 }
